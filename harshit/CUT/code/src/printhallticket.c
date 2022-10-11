43942914cb0a65c647ab6e77146b2f5a36a58f62
hagarg123 <114971064+hagarg123@users.noreply.github.com>
2022-10-11 11:53:07 +0530

    Add files via upload

---
+++ harshit/CUT/code/src/printhallticket.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../header/struct.h"
+
+#define SUCCESS 1
+#define FAILURE 0
+
+extern struct candidate *head_candidate;
+extern struct candidate *tail_candidate;
+
+int printhallticket(char *cid) 
+{	
+	struct candidate* temp = NULL;
+
+	for(temp = head_candidate; temp != NULL; temp = temp->next) // traversing candidate linked list using for loop
+   	{	
+		if(strcmp(temp->candidateid, cid) == 0)         //comparing candidate id from the list of valid candidate
+		{
+         		printf("\n\nNAME           :  %s\n\nCANDIDATE ID   :  %s\n\nEXAM ID        :  %s\n\nEXAM CENTER    :  %s\n\nCOLLEGE        :  %s\n\nADDRESS        :  %s\n\nSTART DATE     :  %s\n\nEND DATE       :  %s",temp->name, temp->candidateid, temp->examid, temp->examcno, temp->college_name, temp->address, temp->start_date, temp->end_date);
+			return SUCCESS;
+		}
+   	}
+	
+	return FAILURE;	
+}
---
+++ harshit/CUT/code/src/sunny_rainy_testcase.c
@@ -0,0 +1,61 @@
+#include "CUnit/Basic.h"
+#include "../header/functions.h"
+
+/* The suite initialization function.
+ * Opens the temporary file used by the tests.
+ * Returns zero on success, non-zero otherwise.
+ */
+
+int init_suite_insertcandidatedata(void) 
+{
+	return 0;
+}
+/* The suite cleanup function.
+ * Closes the temporary file used by the tests.
+ * Returns zero on success, non-zero otherwise.
+ */
+int clean_suite_insertcandidatedata(void) 
+{
+	return 0;
+}
+
+void insertcandidatedata_sunnycases(void) 
+{
+  	CU_ASSERT_EQUAL(insertcandidatedata("C001,Harshit Garg,MH-CET,21/08/2022,25/08/2022"), 1);
+	CU_ASSERT_EQUAL(insertcandidatedata("C004,Sudhanshu sadhwani,UPSC,01/08/2022,01/08/2022"), 1);
+	CU_ASSERT_EQUAL(insertcandidatedata("B010,Subhashree,UPSC,01/08/2022,01/08/2022"), 1);
+	CU_ASSERT_EQUAL(insertcandidatedata("B002,Subhadeep,UPSC,01/08/2022,01/08/2022"), 1);
+	CU_ASSERT_EQUAL(insertcandidatedata("A004,Sudarshan Timisha,CAT,15/10/2022,15/10/2022"), 1);
+}
+
+void insertcandidatedata_rainycases(void) 
+{
+	CU_ASSERT_EQUAL(insertcandidatedata("A012,Ayeshkanta,SAT,01/09/2022,03/04/2023"), 0);
+    	CU_ASSERT_EQUAL(insertcandidatedata("Nikita,"), 0);
+    	CU_ASSERT_EQUAL(insertcandidatedata("A011,Yuvraj,BITSAT,02/03/2023,16/03/2023"), 0);
+    	CU_ASSERT_EQUAL(insertcandidatedata("C023,Laxmi kant,BITSAT,02/03/2023,16/03/2023"), 0);
+    	CU_ASSERT_EQUAL(insertcandidatedata("Jonas Edward,,02/03/2023,16/03/2023"), 0);
+}
+
+int init_suite_printhallticket(void) 
+{
+	return 0;
+}
+
+int clean_suite_printhallticket(void) 
+{
+	return 0;
+}
+
+void printhallticket_sunnycases(void)
+{
+	CU_ASSERT_EQUAL(printhallticket("A004"), 1);
+	CU_ASSERT_EQUAL(printhallticket("B002"), 1);
+	CU_ASSERT_EQUAL(printhallticket("C001"), 1);
+}
+
+void printhallticket_rainycases(void)
+{
+	CU_ASSERT_EQUAL(printhallticket("A011"), 0);
+	CU_ASSERT_EQUAL(printhallticket("B005"), 0);
+}
